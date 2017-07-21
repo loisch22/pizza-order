017f04ef81a3010ba5515621ff47140e0d3331cb
Lois Choi <loisch22@gmail.com>
2017-07-21 11:09:44 -0700

    complete adding and showing total based on size only

--- js/scripts.js
+++ js/scripts.js
@@ -4,41 +4,38 @@ function Pizza(selectSize, toppings, total) {
   this.total = total;
 }
 
-Pizza.prototype.getTotal = function() {
-  if (this.selectSize == "Small" || this.selectSize == "Medium") {
-    basePrice += 2;
-    return basePrice;
-  } else if (this.selectSize == "Large" || this.selectSize == "X-Large") {
-    basePrice += 4;
-    return basePrice;
+Pizza.prototype.getSizeTotal = function(sizeInput) {
+  if (sizeInput === "Small" || sizeInput === "Medium") {
+    var subtotal = this.total += 2;
+    var priceOutput = basePrice + subtotal;
+    return priceOutput;
+  } else if (sizeInput === "Large" || sizeInput === "X-Large") {
+    var subtotal = this.total += 4;
+    var priceOutput = basePrice + subtotal;
+    return priceOutput;
   }
 }
 
 var basePrice = 5;
 
-
-
-
-
-
 $(document).ready(function() {
   $("form#toppings").submit(function(event) {
     event.preventDefault();
-var sizeInput = $("#pizzaSize").val();
-  var chosenToppings = $("input:checkbox[name=select-toppings]:checked").val();
+    debugger;
+    var sizeInput = $("#pizzaSize").val();
+    var chosenToppings = $("input:checkbox[name=select-toppings]:checked").val();
+    var newOrder = new Pizza(sizeInput, chosenToppings, 0);
+    var newOrderSize = newOrder.getSizeTotal(sizeInput);
 
-    $("#pizzaSize").each(function() {
-      // var sizeInput = $(this).val();
-      $('.orderList').append("<strong>" + "Size select: " + "</strong>" + sizeInput);
-    });
-debugger;
+
+
+    $('.orderList').append("<strong>" + "Size select: " + "</strong>" + sizeInput);
     $("input:checkbox[name=select-toppings]:checked").each(function() {
-      // var chosenToppings = $(this).val();
+      chosenToppings = $(this).val();
       $('.orderList').append("<li>" + chosenToppings + "</li>");
-
-      var newOrder = new Pizza(sizeInput, chosenToppings, 0);
-      console.log(newOrder);
-      newOrder.getTotal();
     });
+    $('.orderList').append("Total: " + newOrderSize);
+
+
   });
 });
