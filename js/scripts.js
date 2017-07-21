3e5d46be16d8b1d31785b01cfacb11bcacc8eb4b
Lois Choi <loisch22@gmail.com>
2017-07-21 09:59:38 -0700

    work on spec 3

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,15 @@ function Pizza(selectSize, toppings, total) {
   this.total = total;
 }
 
+Pizza.prototype.getTotal = function() {
+  if (this.selectSize == "Small" || this.selectSize == "Medium") {
+    basePrice += 2;
+    return basePrice;
+  } else if (this.selectSize == "Large" || this.selectSize == "X-Large") {
+    basePrice += 4;
+    return basePrice;
+  }
+}
 
 var basePrice = 5;
 
@@ -15,15 +24,21 @@ var basePrice = 5;
 $(document).ready(function() {
   $("form#toppings").submit(function(event) {
     event.preventDefault();
+var sizeInput = $("#pizzaSize").val();
+  var chosenToppings = $("input:checkbox[name=select-toppings]:checked").val();
 
     $("#pizzaSize").each(function() {
-      var sizeInput = $(this).val();
-      $('.orderList').append("<li>" + sizeInput + "</li>");
+      // var sizeInput = $(this).val();
+      $('.orderList').append("<strong>" + "Size select: " + "</strong>" + sizeInput);
     });
-
+debugger;
     $("input:checkbox[name=select-toppings]:checked").each(function() {
-      var chosenToppings = $(this).val();
+      // var chosenToppings = $(this).val();
       $('.orderList').append("<li>" + chosenToppings + "</li>");
+
+      var newOrder = new Pizza(sizeInput, chosenToppings, 0);
+      console.log(newOrder);
+      newOrder.getTotal();
     });
   });
 });
